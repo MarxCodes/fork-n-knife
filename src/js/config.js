4df5c3d1f5d46a685b19a7b4ddc35cc6726e2470
Marc Shepperd <marc.shepperd07@gmail.com>
2022-07-05 18:53:48 +0100

    final commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '59e4ae0c-077e-4f22-bf6f-526f33e2bf12';
+export const MODAL_CLOSE_SEC = 2.5;
